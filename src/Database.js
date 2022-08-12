2e3368f7454c675018478bc8fe3b21759c3af3a6
clement <clementdu954@live.fr>
2022-08-12 15:17:25 +0200

    cors

--- src/Database.js
+++ src/Database.js
@@ -1,8 +1,23 @@
 import dotenv from "dotenv";
 dotenv.config();
 import mysql from "mysql";
+import app from "express";
 
 const { DB_USER, DB_NAME, DB_HOST, DB_PASSWORD, DB_PORT } = process.env;
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+app.use(cors());
 
 const db = mysql.createConnection({
   user: DB_USER,

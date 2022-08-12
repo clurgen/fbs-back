e8cddc4aaa5924825da493d4416c359e7631dbaf
clement <clementdu954@live.fr>
2022-08-12 15:39:32 +0200

    test

--- src/Database.js
+++ src/Database.js
@@ -17,7 +17,7 @@ app.use((req, res, next) => {
   );
   next();
 });
-app.use(cors());
+app.use(cors({ origin: true }));
 
 const db = mysql.createConnection({
   user: DB_USER,
--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
   );
   next();
 });
-app.use(cors());
+app.use(cors({ origin: true }));
 app.use("/", router);
 app.use(cookieParser());
 app.use("/src/images", express.static("src/images"));

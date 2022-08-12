300c9436497eea3dc5531dcd1bdaea996ba4cb1b
clement <clementdu954@live.fr>
2022-08-12 14:59:53 +0200

    cors correction

--- src/index.js
+++ src/index.js
@@ -9,6 +9,18 @@ dotenv.config();
 const { APP_PORT } = process.env;
 const app = express();
 app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
 app.use(cors());
 app.use("/", router);
 app.use(cookieParser());

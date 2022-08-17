e18fdef97e6f8740991cc42efb953da65c055186
clement <clementdu954@live.fr>
2022-08-17 22:57:47 +0200

    heroku createPool vs createConnect

--- src/Database.js
+++ src/Database.js
@@ -4,7 +4,7 @@ import mysql from "mysql";
 
 const { DB_USER, DB_NAME, DB_HOST, DB_PASSWORD, DB_PORT } = process.env;
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   user: DB_USER,
   host: DB_HOST,
   password: DB_PASSWORD,
